e3871bdb56743f340ed1d6653f9398580d329395
Derek Kastner <dkastner@gmail.com>
2009-04-15 22:38:11 -0400

    Customized global include file

--- config/global.inc.php
+++ config/global.inc.php
@@ -1,14 +1,13 @@
 <?php
-function file_join(*args) {
-  return join(DIRECTORY_SEPARATOR,func_get_args());
-}
-
 error_reporting(E_ALL | E_STRICT);
 set_include_path(
   get_include_path()
-  . PATH_SEPARATOR . file_join(dirname(__FILE__),'..','vendor','konstrukt','lib');
+  . PATH_SEPARATOR . dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.
+                     'vendor'.DIRECTORY_SEPARATOR.'konstrukt'.DIRECTORY_SEPARATOR.'lib'
+  . PATH_SEPARATOR . dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib');
 
-require_once file_join('konstrukt','konstrukt.inc.php');
+require_once 'id_rather_be_coding_ruby';
+require_once File.join('konstrukt','konstrukt.inc.php');
 
 date_default_timezone_set('US/Eastern');
 set_error_handler('k_exceptions_error_handler');
