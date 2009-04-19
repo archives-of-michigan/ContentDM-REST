632da00b1e13b70fa829158365bef8ec6d4a644e
Derek Kastner <dkastner@gmail.com>
2009-04-19 16:07:53 -0400

    Using ruby-like convenience methods

--- config/global.inc.php
+++ config/global.inc.php
@@ -1,12 +1,16 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
-set_include_path(
-  get_include_path()
-  . PATH_SEPARATOR . dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.
-                     'vendor'.DIRECTORY_SEPARATOR.'konstrukt'.DIRECTORY_SEPARATOR.'lib'
-  . PATH_SEPARATOR . dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib');
+require_once join(DIRECTORY_SEPARATOR, array(dirname(__FILE__),'..','lib','id_rather_be_coding_ruby'));
 
-require_once 'id_rather_be_coding_ruby';
+set_include_path(
+  get_include_path().join(PATH_SEPARATOR,
+    array(
+      File.join(File.dirname(__FILE__),'..','vendor','konstrukt','lib'),
+      File.join(File.dirname(__FILE__),'..','lib')
+    )
+  )
+);
+
 require_once File.join('konstrukt','konstrukt.inc.php');
 
 date_default_timezone_set('US/Eastern');
--- lib/id_rather_be_coding_ruby.php
+++ lib/id_rather_be_coding_ruby.php
@@ -1,15 +1,15 @@
 <?
 class File {
-  public static function join(*args) {
+  public static function join() {
     return join(DIRECTORY_SEPARATOR,func_get_args());
   }
   
-  public static function dirname(path) {
-    return dirname(path);
+  public static function dirname($path) {
+    return dirname($path);
   }
   
-  public static function exist(path) {
-    return file_exists(path);
+  public static function exist($path) {
+    return file_exists($path);
   }
 }
 ?>
