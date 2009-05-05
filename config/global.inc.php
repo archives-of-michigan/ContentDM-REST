b522a8d2e57715ce1936a63154550266501ed4b6
Derek Kastner <dkastner@gmail.com>
2009-05-04 20:32:34 -0400

    Added project root to path, fixed path separator

--- config/global.inc.php
+++ config/global.inc.php
@@ -3,10 +3,12 @@ error_reporting(E_ALL | E_STRICT);
 require_once join(DIRECTORY_SEPARATOR, array(dirname(__FILE__),'..','lib','id_rather_be_coding_ruby.php'));
 
 set_include_path(
-  get_include_path().join(PATH_SEPARATOR,
+  get_include_path().PATH_SEPARATOR.
+  join(PATH_SEPARATOR,
     array(
       file_join(dirname(__FILE__),'..','vendor','konstrukt','lib'),
-      file_join(dirname(__FILE__),'..','lib')
+      file_join(dirname(__FILE__),'..','lib'),
+      file_join(dirname(__FILE__),'..')
     )
   )
 );
