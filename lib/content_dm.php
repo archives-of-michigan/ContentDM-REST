ccedf4fed675c9536539113744261ca1b854295b
Derek Kastner <dkastner@gmail.com>
2009-05-04 21:54:01 -0400

    Fixed include paths

--- lib/content_dm.php
+++ lib/content_dm.php
@@ -1,11 +1,10 @@
 <?php
 
-define(DMSCRIPTS_PATH, file_join(dirname(__FILE__),'..','..','dmscripts'));
+define('DMSCRIPTS_PATH', file_join(dirname(__FILE__),'..','..','dmscripts'));
 
 if(file_exists(DMSCRIPTS_PATH)) {
   require file_join(DMSCRIPTS_PATH,"DMSystem.php");
   require file_join(DMSCRIPTS_PATH,"DMImage.php");
 } else {
-  echo "USING TEST FIXTURES FOR CONTENTDM";
-  require file_join(dirname(__FILE__),'..','..','test','fixtures','content_dm_api');
+  require file_join(dirname(__FILE__),'..','test','fixtures','content_dm_api.php');
 }
