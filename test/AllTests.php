8b6a47f2470cc4f6bfe88391d1bdf19ae9136027
Derek Kastner <dkastner@gmail.com>
2009-05-07 23:42:01 -0400

    Unit tests for Collection class

--- test/AllTests.php
+++ test/AllTests.php
@@ -2,16 +2,23 @@
 require_once join(DIRECTORY_SEPARATOR, array(dirname(__FILE__),'test_helper.php'));
 
 require_once file_join('PHPUnit','Framework.php');
-require_once file_join(dirname(__FILE__),'lib','CollectionsGateway','CollectionsGatewaySelectAllTest.php');
 require_once file_join(dirname(__FILE__),'lib','models','collection','CollectionTest.php');
-require_once file_join(dirname(__FILE__),'lib','models','collection','CollectionFieldInfoTest.php');
- 
+require_once file_join(dirname(__FILE__),'lib','models','collection','CollectionCalculateZoomLevelsTest.php');
+require_once file_join(dirname(__FILE__),'lib','models','collection','CollectionImageSettingsTest.php');
+require_once file_join(dirname(__FILE__),'lib','models','collection','CollectionAllTest.php');
+require_once file_join(dirname(__FILE__),'lib','models','collection','field','CollectionFieldInitializeTest.php');
+require_once file_join(dirname(__FILE__),'lib','models','collection','field','CollectionFieldFindAllByAliasTest.php');
+
 class AllTests {
   public static function suite() {
     $suite = new PHPUnit_Framework_TestSuite;
-    $suite->addTestSuite('CollectionsGatewaySelectAllTest');
     $suite->addTestSuite('CollectionTest');
-    $suite->addTestSuite('CollectionFieldInfoTest');
+    $suite->addTestSuite('CollectionCalculateZoomLevelsTest');
+    $suite->addTestSuite('CollectionImageSettingsTest');
+    $suite->addTestSuite('CollectionAllTest');
+    $suite->addTestSuite('CollectionFieldInitializeTest');
+    $suite->addTestSuite('CollectionFieldFindAllByAliasTest');
+    
     return $suite;
   }
 }
