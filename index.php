089dadbe48e531c53d53480741a6aa7242551515
Derek Kastner <dkastner@gmail.com>
2009-05-11 21:17:33 -0400

    Disabling debug mode

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once join(DIRECTORY_SEPARATOR, array(dirname(__FILE__),'config','global.inc.php'));
 k()
   // Uncomment the nexct line to enable in-browser debugging
-  ->setDebug()
+  // ->setDebug()
   // Dispatch request
   ->run('components_Root')
   ->out();
