71ab556e42d966b85eef534cf24940d0dc4c33ca
Derek Kastner <dkastner@gmail.com>
2009-08-11 22:18:10 -0400

    Enabled front controller mode

--- index.php
+++ index.php
@@ -2,14 +2,11 @@
 ini_set('display_errors', 1);
 
 require_once join(DIRECTORY_SEPARATOR, array(dirname(__FILE__),'config','global.inc.php'));
-require 'url_parser.php';
-
-$_SERVER['REQUEST_URI'] = URLParser::parse_uri($_SERVER['QUERY_STRING']);
-$_SERVER['QUERY_STRING'] = URLParser::parse_query_string($_SERVER['REQUEST_URI']);
 
 k()
   // Uncomment the nexct line to enable in-browser debugging
   // ->setDebug()
+  ->enableFrontController()
   // Dispatch request
   ->run('components_Root')
   ->out();
